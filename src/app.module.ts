6d6b9184dcb9fc06b62a27f64f72265bbec56383
Aleksei Arro <polsck@gmail.com>
2021-07-24 19:55:41 +0300

    build: setup PostgreSQL connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Cars } from './cars/entities/car.entity';
 import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [CarsModule],
+  imports: [
+    CarsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'guest123',
+      database: 'transportation',
+      entities: [Cars],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/cars/entities/car.entity.ts
+++ src/cars/entities/car.entity.ts
@@ -1,1 +1,16 @@
-export class Car {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Cars {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text')
+  make: string;
+
+  @Column('text')
+  model: string;
+
+  @Column('int')
+  miles: number;
+}
