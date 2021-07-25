98272390013defd5a0902c8bc20ddd6996a536ef
Aleksei Arro <polsck@gmail.com>
2021-07-25 22:13:54 +0300

    feat: add Flight model

---
+++ packages/frontend/src/app/flight.model.ts
@@ -0,0 +1,8 @@
+export type TFlight = {
+  origin: string;
+  destination: string;
+  flightNumber: number;
+  depart: string;
+  arrive: string;
+  nonstop: boolean;
+};
--- packages/frontend/src/app/home/home.component.ts
+++ packages/frontend/src/app/home/home.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FlightsService } from '../flights.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private flightsService: FlightsService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  getFlights() {}
 }
