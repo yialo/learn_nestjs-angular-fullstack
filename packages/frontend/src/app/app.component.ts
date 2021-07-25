cc846a7e2844e44284327dcbf407f3e37aae49ce
Aleksei Arro <polsck@gmail.com>
2021-07-25 21:57:23 +0300

    feat: add page layout and main navigation

--- packages/frontend/src/app/app.component.ts
+++ packages/frontend/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+type TNavLink = {
+  path: string;
+  label: string;
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  navLinks: TNavLink[] = [
+    { path: '', label: 'Home' },
+    { path: 'admin', label: 'Admin' },
+  ];
+  year = new Date().getFullYear();
 }
